5fb54374281f385693120366640b80934e49ac14
Andrew Bramley <andrew@tesora.com>
2014-08-12 16:06:59 -0400

    Add support for Trove Replication
    Added initial support for replication.
    Changed launch dialog to have an Advanced tab where
    you can select a master instance for replication
    Display replication information in details view
    Added support for Detach
    
    Change-Id: I2c90d5be0f47c07fe9049a685fbf7a36a2585e0e
    Implements: blueprint trove-replication-v1-support
    
    Conflicts:
    	openstack_dashboard/dashboards/project/databases/templates/databases/_detail_overview.html
    	openstack_dashboard/dashboards/project/databases/tests.py
    	requirements.txt

--- openstack_dashboard/dashboards/project/databases/tables.py
+++ openstack_dashboard/dashboards/project/databases/tables.py
@@ -58,6 +58,22 @@ class RestartInstance(tables.BatchAction):
         api.trove.instance_restart(request, obj_id)
 
 
+class DetachReplica(tables.BatchAction):
+    name = "detach_replica"
+    action_present = _("Detach")
+    action_past = _("Scheduled detatchment of %(data_type)s")
+    data_type_singular = _("Replica")
+    data_type_plural = _("Replicas")
+    classes = ('btn-danger', 'btn-detach-replica')
+
+    def allowed(self, request, instance=None):
+        return (instance.status in ACTIVE_STATES
+                and hasattr(instance, 'replica_of'))
+
+    def action(self, request, obj_id):
+        api.trove.instance_detach_replica(request, obj_id)
+
+
 class DeleteUser(tables.DeleteAction):
     data_type_singular = _("User")
     data_type_plural = _("Users")
@@ -199,7 +215,7 @@ class InstancesTable(tables.DataTable):
     STATUS_CHOICES = (
         ("ACTIVE", True),
         ("BLOCKED", True),
-        ("BUILD", True),
+        ("BUILD", None),
         ("FAILED", False),
         ("REBOOT", None),
         ("RESIZE", None),
@@ -239,6 +255,7 @@ class InstancesTable(tables.DataTable):
                        ResizeVolume,
                        ResizeInstance,
                        RestartInstance,
+                       DetachReplica,
                        TerminateInstance)
 
 
