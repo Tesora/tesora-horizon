a00c2362c1493c2a198383eb9dc90ca9ef422026
WonChon Jung <arisu1000@gmail.com>
2015-06-04 06:36:02 +0900

    Support flavor-id uuid format at trove.
    Change-Id: I78997209a78425ac690c58dbd2d25bc126bcae41
    Closes-Bug: 1453398

--- openstack_dashboard/contrib/trove/api/trove.py
+++ openstack_dashboard/contrib/trove/api/trove.py
@@ -216,7 +216,10 @@ def backup_create(request, name, instance_id, description=None,
 
 
 def flavor_list(request):
-    return troveclient(request).flavors.list()
+    flavors = troveclient(request).flavors.list()
+    for flavor in flavors:
+        flavor.id = flavor.str_id
+    return flavors
 
 
 def flavor_get(request, flavor_id):
