2fde2535c85555692dd0d7615e2c5e4377bc8c05
Andrew Bramley <andrew@tesora.com>
2016-02-16 11:58:05 -0500

    Remove cisco folder from repo
    It is now in tesora-trove-dbaas-packaging
    
    Change-Id: Ie7976c8d7c34f7e1cb0319dd0c169f1d783e85f7

--- cisco/files/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="tesora_horizon",
-    version="1.0.0",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    package_data={'': ['**.html', ], },
-)
